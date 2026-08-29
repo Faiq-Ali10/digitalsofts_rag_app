63b81400993b0d5b77864d2c5868c9334138d7e9
Faiq-Ali10 <faiqalio1oo@gmail.com>
2026-08-29 19:52:58 +0500

    style: remove trailing whitespace in CORS config

--- backend/app/main.py
+++ backend/app/main.py
@@ -71,7 +71,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000", 
+        "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://204.216.106.54:3000",
         "http://204.216.106.54"
